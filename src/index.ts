71a2a3a8d7a9a5abc59f65118d762510aed89d8a
nsinghROLO <nsingh@rodeologistics.co>
2020-11-10 23:20:47 -0600

    Added mikro-orm config. Added packages express apollo-server-express graphql type-graphql. Added scripts for typescipt-watch.

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,15 @@
 import { MikroORM } from '@mikro-orm/core';
 import { __prod__ } from './constants';
+import { Post } from './entities/Post';
+import microConfig from './mikro-orm.config';
 
 const main = async () => {
-    const orm = await MikroORM.init({
-        entities: []
-        dbName: 'react_graphql',
-        type: 'postgresql',
-        debug: !__prod__,
-    });
+    const orm = await MikroORM.init(microConfig);
+    await orm.getMigrator().up(); // runs the migrations before it gets anuthing up.
+    const post = await orm.em.create(Post, { title: 'My First Post' });
+    await orm.em.persistAndFlush(post);
+    // const posts = await orm.em.find(Post, {});
+    console.log(post);
 };
 
 main();
