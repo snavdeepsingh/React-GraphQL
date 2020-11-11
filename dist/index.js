531813f25e9bbe83a4e095e9c2e11ed38f007dcd
nsinghROLO <nsingh@rodeologistics.co>
2020-11-11 14:20:29 -0600

    Added express types.

--- dist/index.js
+++ dist/index.js
@@ -13,14 +13,20 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const core_1 = require("@mikro-orm/core");
-const Post_1 = require("./entities/Post");
 const mikro_orm_config_1 = __importDefault(require("./mikro-orm.config"));
+const express_1 = __importDefault(require("express"));
 const main = () => __awaiter(void 0, void 0, void 0, function* () {
     const orm = yield core_1.MikroORM.init(mikro_orm_config_1.default);
     yield orm.getMigrator().up();
-    const post = yield orm.em.create(Post_1.Post, { title: 'My First Post' });
-    yield orm.em.persistAndFlush(post);
-    console.log(post);
+    const app = express_1.default();
+    app.get('/', (_, res) => {
+        res.send('HELLO WORLD');
+    });
+    app.listen(4000, () => {
+        console.log('server listening on localhost:4000');
+    });
 });
-main();
+main().catch(err => {
+    console.log(err);
+});
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -2,14 +2,21 @@ import { MikroORM } from '@mikro-orm/core';
 import { __prod__ } from './constants';
 import { Post } from './entities/Post';
 import microConfig from './mikro-orm.config';
+import express from 'express';
 
 const main = async () => {
     const orm = await MikroORM.init(microConfig);
     await orm.getMigrator().up(); // runs the migrations before it gets anuthing up.
-    const post = await orm.em.create(Post, { title: 'My First Post' });
-    await orm.em.persistAndFlush(post);
-    // const posts = await orm.em.find(Post, {});
-    console.log(post);
+    
+    const app = express();
+    app.get('/', (_, res) => {
+        res.send('HELLO WORLD');
+    })
+    app.listen(4000, () => {
+        console.log('server listening on localhost:4000');
+    })
 };
 
-main();
+main().catch(err => {
+    console.log(err);
+})
