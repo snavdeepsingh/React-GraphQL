5e22464a4faeb90c935743e4b85ddccb08db4080
nsinghROLO <nsingh@rodeologistics.co>
2020-11-11 22:16:33 -0600

    Added mutation to create and update posts.

--- src/resolvers/post.ts
+++ src/resolvers/post.ts
@@ -1,5 +1,5 @@
 import { MyContext } from 'src/types';
-import { Arg, Ctx, Int, Query, Resolver } from 'type-graphql';
+import { Arg, Ctx, Int, Mutation, Query, Resolver } from 'type-graphql';
 import { Post } from '../entities/Post';
 
 @Resolver()
@@ -17,5 +17,34 @@ export class PostResolver {
     @Ctx() {em}: MyContext
   ): Promise <Post | null> {
     return em.findOne(Post, { id });
+  }
+
+  @Mutation(() => Post)
+  async createPost(
+    @Arg('title') title: string,
+    @Ctx() {em}: MyContext
+  ): Promise<Post> {
+    const post = em.create(Post, { title });
+    await em.persistAndFlush(post);
+    return post;
+  }
+
+  @Mutation(() => Post, { nullable: true})
+  async updatePost(
+    @Arg("id") id: number,
+    @Arg("title", () => String, { nullable: true}) title: string,
+    @Ctx() {em}: MyContext
+  ): Promise<Post | null>{
+    const post = await em.findOne(Post, { id });
+    if (!post) {
+      return null;
+    }
+
+    if (typeof title !== 'undefined') {
+      post.title = title;
+      await em.persistAndFlush(post);
+    }
+
+    return post;
   }
 }
