87a332d24a9e0f7295d471b7c083c601ee317573
nsinghROLO <nsingh@rodeologistics.co>
2020-11-11 21:51:58 -0600

    Added module reflect-metadata. Added Apollo-express-graphql config. Entity Post. Resolver for Post schema with getAll and findOne queries.

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,32 @@
+import "reflect-metadata";
 import { MikroORM } from '@mikro-orm/core';
 import { __prod__ } from './constants';
 import { Post } from './entities/Post';
 import microConfig from './mikro-orm.config';
 import express from 'express';
+import { ApolloServer } from 'apollo-server-express';
+import { buildSchema } from 'type-graphql';
+import { HelloResolver } from './resolvers/hello';
+import { PostResolver } from './resolvers/post';
 
 const main = async () => {
     const orm = await MikroORM.init(microConfig);
     await orm.getMigrator().up(); // runs the migrations before it gets anuthing up.
     
     const app = express();
-    app.get('/', (_, res) => {
-        res.send('HELLO WORLD');
+
+    const apolloServer = new ApolloServer({
+        schema: await buildSchema({
+            resolvers: [
+                HelloResolver,
+                PostResolver,
+            ],
+            validate: false
+        }),
+        context: () => ({ em: orm.em })
     })
+
+    apolloServer.applyMiddleware({ app })
     app.listen(4000, () => {
         console.log('server listening on localhost:4000');
     })
